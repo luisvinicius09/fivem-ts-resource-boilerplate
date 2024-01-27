31a5db6dce4507fdb31d5c7c2ddbc60815046ecb
Luis V <luisvinicius0906@gmail.com>
2024-01-27 19:36:33 -0300

    feat(manifest): add manifest file

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'luisvinicius09'
+description 'boilerplate'
+version '1.0.0'
+
+client_script 'dist/client.js'
+server_script 'dist/server.js'
+
+-- dependencies {
+--   'yarn'
+-- }
+
+-- ui_page ''
+-- file ''
+
+-- server_only 'yes'
